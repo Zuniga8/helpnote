bf74a5b091a6e96bef4a7f4613ae71b837b9f7af
GZ <gabriel.zuni8@gmail.com>
2023-01-26 23:58:54 -0300

    commit inicial 2.0

---
+++ js/script.js
@@ -0,0 +1,142 @@
+import { listSubStatus } from "./templates.js";
+import { listBadleads } from './templates.js';
+import { scriptProntos } from "./templates.js";
+
+//      Time
+const t = new Date();
+const txt_date = t.getDate() + "/" + (t.getMonth() + 1) + "/" + t.getFullYear()
+
+//      Menu Note
+let opt = document.querySelectorAll('.opt')
+document.querySelectorAll('[name="options"]').forEach(function (op) {
+    op.addEventListener('click', function () {
+        for (let x = 0; x < opt.length; x++) {
+            if (op.value == opt[x].classList[1]) {
+                opt[x].classList.toggle("d-none")
+            } else {
+                opt[x].classList.remove("d-none")
+                opt[x].classList.toggle("d-none")
+            }
+        }
+    })
+})
+//      Menu Note
+
+//          Textarea {
+let textEnter = document.querySelectorAll(".txtarea");
+textEnter.forEach(function (txt) {
+    txt.addEventListener("input", calcTextarea
+    );
+});
+function calcTextarea() {
+    this.style.height = "auto";
+    this.style.height = this.scrollHeight + "px";
+}
+//     } Fim Textarea
+
+
+//      sub Status
+let recebess = ''
+listSubStatus.forEach(function (ss) {
+    recebess = recebess + "<option>" + ss + "</option>";
+})
+document.querySelector('#txt_subStatus').innerHTML = recebess
+//      Fim sub Status
+
+
+//      bad Leads
+{
+    let exibeBadlead = ''
+    listBadleads.forEach(function (bl, n) {
+        exibeBadlead = exibeBadlead +
+            '<li><input type="checkbox" name="menuopt" id="drop' + n + '" value="' + bl + '"/>' +
+            '<label class="open" for="drop' + n + '">' + bl + '</label></li>'
+    })
+    document.querySelector('.badLeads').innerHTML = exibeBadlead
+
+    let checkbl = []
+    document.querySelectorAll('input[name="menuopt"]').forEach(function (blc) {
+        blc.addEventListener('click', function () {
+            if (blc.checked == true) {
+                checkbl.push(blc.value)
+            } else if (blc.checked == false) {
+                let n = checkbl.indexOf(blc.value)
+                checkbl.splice(n, 1)
+            }
+        })
+    })
+}//      Fim badleads
+
+
+// BTN limpar dados 
+/*
+document.querySelector('.limpaform').addEventListener('click', function () {
+    document.querySelector("form").reset();
+    document.querySelector(".lbl").innerHTML = "";
+    checkbl = []
+})*/
+// Fim BTN limpar dados
+
+//          gera note
+
+//      btn menu preview
+
+let form = document.querySelector('#formNote')
+let preview = document.querySelector('.preview')
+document.querySelector('.menuNote>div>input[name="option"]#checkPreview').addEventListener('click', function (ch) {
+
+    if (ch.target.checked) {
+        console.log('ok')
+        form.classList.toggle('d-none')
+        preview.classList.toggle('d-none')
+        GerarNote()
+    } else {
+        form.classList.toggle('d-none')
+        preview.classList.toggle('d-none')
+    }
+})
+// gera note
+function GerarNote() {
+    console.log('click beranote')
+    let imprime = '' + "<p class='pb-0 mb-2 text-break'><span class='fw-bold'>Data </span>" + txt_date + "</p>";
+    let x = 0
+    let form = document.querySelector('#formNote')
+    form.querySelectorAll('div.row > div> label').forEach(function (lbl, id) {
+        let registra = ''
+        //console.log(id + '- ' + lbl.innerText)
+        if (id === 1) {
+            opt = document.querySelector('#txt_subStatus').selectedIndex
+            registra = document.querySelectorAll('#txt_subStatus>option')[opt].innerText
+        } else if (id === 4 || id === 5 || id === 6 || id === 3) {
+            registra = "<br>" + form.querySelectorAll('textarea')[id - 3].value.replaceAll("\n", "<br>");
+        } else if (id == 8) {
+            document.querySelectorAll("[name='menuopt']").forEach(function (opt) {
+                if (opt.checked) {
+                    registra = registra + '<br>' + opt.value
+                }
+            })
+        }
+        else {
+            registra = form.querySelectorAll('input.form-control')[x].value
+            x += 1
+        }
+
+        imprime = imprime +
+            "<p class='pb-0 mb-2 text-break'><span class='fw-bold'>" +
+            lbl.innerText +
+            '</span>  ' +
+            registra +
+            "</p>";
+    })
+    document.querySelector("#notePreview").innerHTML = imprime;
+}
+//          gera note
+
+document.querySelector('.copyNote').addEventListener('click', function () {
+ 
+    form.classList.toggle('d-none')
+    preview.classList.toggle('d-none')
+    GerarNote()
+})
+
+
